801389d146dfee6c95dca865c3879753abf91f12
Md. Awadhe Hossain <149267112+awadhehossain@users.noreply.github.com>
2026-08-12 01:20:44 +0600

    Katryoshka edit

--- Condition/katryoshka.c
+++ Condition/katryoshka.c
@@ -1,25 +1,17 @@
-#include<stdio.h>
-int main()
-{
-    long long int n,m,k;
-    scanf("%lld %lld %lld",&n,&m,&k);
-    long long count =0;
-    while(n>=1 && m>=1 && k>=1)
-    {
-        n--;
-        m--;
-        k--;
-        count++;
-    }
-
-    while(n>=2 && k>=1)
-    {
-        n-=2;
-        k--;
-        count++;
-    }
-
-    printf("%lld\n",count);
+#include <stdio.h>
+ 
+int main() {
+    long long n, m, k;
+    scanf("%lld %lld %lld", &n, &m, &k);
+ 
+    long long limit1 = n;
+    long long limit2 = k;
+    long long limit3 = (n + m) / 2;
+ 
+    long long ans = limit1;
+    if (limit2 < ans) ans = limit2;
+    if (limit3 < ans) ans = limit3;
+ 
+    printf("%lld\n", ans);
     return 0;
-
 }
